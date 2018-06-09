6a3f40c94fa8ccfb85dd2781b4e2bcfc22757811
Claytonwally <wallyworld1ac@gmailcom>
2018-06-09 03:04:37 -0400

    finally put all examples together and they are working properly

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,10 @@
 for (let i = 1; i <= 100; i++) {
-  if (i % 15 == 0) {
-    console.log('fizzBuzz');
+  if (i % 5 == 0 && i % 3 == 0) {
+    console.log(i + 'fizzBuzz');
   }else if (i % 3 == 0) {
-    console.log('fizz');
+    console.log(i + 'fizz');
   }else if (i % 5 == 0) {
-    console.log('buzz');
+    console.log(i + 'buzz');
   }else {
     console.log(i);
 
